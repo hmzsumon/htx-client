931cbd81d59afaf7565341a8d4193f99c96da44b
hmzsumon <zsumonn@gmail.com>
2025-03-09 11:20:43 +0600

    first commit

--- components/home/TradeVolume.tsx
+++ components/home/TradeVolume.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const TradeVolume = () => {
 	return (
-		<div className='bg-htx-blue py-6 flex items-center justify-center'>
+		<div className='bg-htx-blue py-10 flex flex-col gap-4 items-center justify-center'>
+			<p className='text-white'>24h Trading Volume</p>
 			<h2 className='text-4xl font-bold text-white'>$2,441,698,048</h2>
 		</div>
 	);
