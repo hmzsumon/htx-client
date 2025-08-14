8e31529ee8f49ed2af81753eec6d8686456d53a0
Zakaria Sumon <zsumonn@gmail.com>
2025-08-15 04:23:39 +0600

    New heroku api added

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'development') {
 	// console.log('Running in development mode');
 } else {
 	//Global-X-api-1901b8e6064a.herokuapp.com/
-	https: baseUrl = 'https://htx-trade-31c631dd8c91.herokuapp.com';
+	https: baseUrl = 'https://htx-trade-02-be257b06281e.herokuapp.com';
 	// Code specific to production mode
 	// console.log('Running in production mode');
 }
