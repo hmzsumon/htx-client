7440a16b4b3e17e3a91fd897b103d48b9f4902a1
Zakaria Sumon <zsumonn@gmail.com>
2025-09-05 22:00:26 +0600

    added ne callBac url

--- components/dashboard/ContactMenu.tsx
+++ components/dashboard/ContactMenu.tsx
@@ -1,44 +1,43 @@
-'use client';
-import React from 'react';
-import DashboardMenuCard from './DashboardMenuCard';
-import Mail from '@/public/images/icons/mail.png';
-import Telegram from '@/public/images/icons/telegram.png';
-import Inbox from '@/public/images/icons/inbox.png';
+"use client";
+import Inbox from "@/public/images/icons/inbox.png";
+import Mail from "@/public/images/icons/mail.png";
+import Telegram from "@/public/images/icons/telegram.png";
+import DashboardMenuCard from "./DashboardMenuCard";
 
 const items = [
-	{
-		id: 4,
-		title: 'Support',
-		icon: null,
-		iconImg: Mail,
-		link: '/contact/support',
-	},
-	{
-		id: 5,
-		title: 'Telegram',
-		icon: null,
-		iconImg: Telegram,
-		link: '/contact/telegram',
-	},
-	{
-		id: 6,
-		title: 'Inbox',
-		icon: null,
-		iconImg: Inbox,
-		link: '/notifications',
-	},
+  {
+    id: 4,
+    title: "Support",
+    icon: undefined,
+    iconImg: Mail,
+    link: "/contact/support",
+  },
+  {
+    id: 5,
+    title: "Telegram",
+    icon: undefined,
+    iconImg: Telegram,
+    link: "/contact/telegram",
+  },
+  {
+    id: 6,
+    title: "Inbox",
+    icon: undefined,
+    iconImg: Inbox,
+    link: "/notifications",
+  },
 ];
 
 const ContactMenu = () => {
-	return (
-		<>
-			<DashboardMenuCard
-				title='Contact Us'
-				items={items}
-				bgColor={'bg-[#e6ddec]'}
-			/>
-		</>
-	);
+  return (
+    <>
+      <DashboardMenuCard
+        title="Contact Us"
+        items={items}
+        bgColor={"bg-[#e6ddec]"}
+      />
+    </>
+  );
 };
 
 export default ContactMenu;
